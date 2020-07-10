47e7bf05dba136fddc38f26581826ff2b03b6ec3
Vatazhyshyn <vatazishin@gmail.com>
2020-07-10 14:04:10 +0300

    finish styling menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import HomePage from "./components/homePage";
+import HomePage from "./pages/homepage/homePage";
 
 function App() {
   return (
---
+++ src/pages/homepage/homePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './HomePage.scss';
+import Directory from "../../components/directory/Diretory";
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+
+export default HomePage;
